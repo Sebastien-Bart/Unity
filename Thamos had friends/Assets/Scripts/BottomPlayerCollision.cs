5b9c838707891d72edcd76cc2d31b6406ce00d86
Sebastien-Bart <45076193+Sebastien-Bart@users.noreply.github.com>
2019-12-24 15:31:32 +0100

    add Thamos had friends

---
+++ Thamos had friends/Assets/Scripts/BottomPlayerCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomPlayerCollision : MonoBehaviour
+{
+    [SerializeField] private PlayerController playerController;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        playerController.setJumpingFalse();
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+
+}
---
+++ Thamos had friends/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private Transform goal;
+
+    private Rigidbody2D rb;
+    private Animation jumpAnimation;
+
+    private bool jumping = false;
+    private bool jumpedPressed;
+    private float horizontalMovement;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jumpAnimation = GetComponent<Animation>();
+    }
+
+    private void Update()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        jumpedPressed = Input.GetButton("Jump");
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalMovement * speed, rb.velocity.y);
+        if (jumpedPressed)
+            Jump();
+    }
+
+    private void Jump()
+    {
+        if(!jumping)
+        {
+            rb.AddForce(new Vector2(0, 1 * jumpForce), ForceMode2D.Impulse);
+            jumping = true;
+            jumpAnimation.Play("jump");
+        }
+    }
+
+    public void setJumpingFalse()
+    {
+        jumping = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Goal")
+        {
+            jumpAnimation.Play("GoalAnimation");
+        }
+    }
+
+}
