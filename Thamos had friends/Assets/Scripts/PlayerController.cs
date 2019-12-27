e9fe8265533f6003014e8f5dce165f56385b2a1e
Sebastien-Bart <45076193+Sebastien-Bart@users.noreply.github.com>
2019-12-27 13:47:23 +0100

    go to center of goal when touched

--- Thamos had friends/Assets/Scripts/PlayerController.cs
+++ Thamos had friends/Assets/Scripts/PlayerController.cs
@@ -1,61 +1,94 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+    
     [SerializeField] private float speed = 10f;
     [SerializeField] private float jumpForce = 10f;
-    [SerializeField] private Transform goal;
+    [SerializeField] private int nbJumps = 1;
+    [SerializeField] private Transform groundCheckTopLeft;
+    [SerializeField] private Transform groundCheckBottomRight;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private GameObject goal;
+
 
     private Rigidbody2D rb;
-    private Animation jumpAnimation;
+    private Animation playerAnimation;
 
+    private bool isGrounded = false;
+    private bool movable = true;
     private bool jumping = false;
-    private bool jumpedPressed;
+    private bool jumpPressed = false;
     private float horizontalMovement;
+    private int currentNbJumps;
+    private bool goalReached = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        jumpAnimation = GetComponent<Animation>();
+        playerAnimation = GetComponent<Animation>();
+        currentNbJumps = nbJumps;
     }
 
     private void Update()
     {
+        isGrounded = Physics2D.OverlapArea(groundCheckTopLeft.position, groundCheckBottomRight.position, layerMask);
         horizontalMovement = Input.GetAxis("Horizontal");
-        jumpedPressed = Input.GetButton("Jump");
+        jumpPressed = Input.GetButtonDown("Jump");
 
+        if (isGrounded)
+        {
+            currentNbJumps = nbJumps;
+            jumping = false;
+        }
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontalMovement * speed, rb.velocity.y);
-        if (jumpedPressed)
-            Jump();
+        if (movable)
+        {
+            rb.velocity = new Vector2(horizontalMovement * speed, rb.velocity.y);
+            if (jumpPressed)
+            {
+                if (isGrounded && currentNbJumps > 0)
+                {
+                    jumping = true;
+                    Jump();
+                }
+                else if (jumping && currentNbJumps > 0) {
+                    Jump();
+                }
+            }
+        }
     }
 
     private void Jump()
     {
-        if(!jumping)
+        rb.AddForce(new Vector2(0, 1 * jumpForce), ForceMode2D.Impulse);
+        jumping = true;
+        playerAnimation.Play("jump");
+        currentNbJumps--;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == goal)
         {
-            rb.AddForce(new Vector2(0, 1 * jumpForce), ForceMode2D.Impulse);
-            jumping = true;
-            jumpAnimation.Play("jump");
+            movable = false;
+            rb.velocity = Vector2.zero;
+            rb.isKinematic = true;
+            StartCoroutine(MoveTo(transform, goal.transform.position, 0.7f));
         }
     }
 
-    public void setJumpingFalse()
+    public static IEnumerator MoveTo(Transform toMove, Vector2 destination, float speed)
     {
-        jumping = false;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Goal")
+        while((Vector2) toMove.position != destination)
         {
-            jumpAnimation.Play("GoalAnimation");
+            toMove.position = (Vector2) Vector2.MoveTowards(toMove.position, destination, speed * Time.deltaTime);
+            yield return null;
         }
     }
-
 }
