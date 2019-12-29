140a3c2cb395d1debae98fbfe6ba878d9606c6e4
Sebastien-Bart <45076193+Sebastien-Bart@users.noreply.github.com>
2019-12-29 16:00:37 +0100

    multiple character in the scene enabled

--- Thamos had friends/Assets/Scripts/PlayerController.cs
+++ Thamos had friends/Assets/Scripts/PlayerController.cs
@@ -12,7 +12,8 @@ public abstract class PlayerController : MonoBehaviour {
     [SerializeField] [Range(1, 20)] protected float speed = 10f;
     [SerializeField] [Range(1, 20)] protected float jumpForce = 10f;
     [SerializeField] [Range(0, 5)] protected int extraJumps = 0;
-    [SerializeField] [Range(0.1f, 0.3f)] protected float jumpTime = 1.0f;
+    [SerializeField] [Range(0.1f, 0.3f)] protected float jumpTime = 0.2f;
+    [SerializeField] [Range(0.1f, 1f)] protected float moveToGoalSpeed = 0.7f;
     [SerializeField] protected Transform groundCheckTopLeft;
     [SerializeField] protected Transform groundCheckBottomRight;
     [SerializeField] protected LayerMask layerMask;
@@ -22,42 +23,54 @@ public abstract class PlayerController : MonoBehaviour {
 
     protected Rigidbody2D rb;
     protected Animation playerAnimation;
-
+    protected SpriteRenderer spriteRenderer;
+    protected Color initialColor;
     
     protected float horizontalMovement;
 
     protected bool onGoal = false;
     protected bool landingPlayed = false;
-    protected bool jumping = false;
-    protected bool isOnFocus = true; // A CHANGER
+    protected bool isOnFocus = false;
     protected bool isGrounded = false;
     protected bool movable = true;
+
+    protected bool jumping = false;
     protected int extraJumpsAvailable;
     protected float jumpTimeCounter;
 
     protected void Start()
     {
-        characterName = giveCharacterName();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         playerAnimation = GetComponent<Animation>();
+        initialColor = spriteRenderer.color;
+        characterName = giveCharacterName();
         extraJumpsAvailable = extraJumps;
         jumpTimeCounter = jumpTime;
+
+        //set first in the list onFocus
+        transform.parent.GetChild(0).GetComponent<PlayerController>().setOnFocus();
     }
 
     protected void Update()
     {
         isGrounded = Physics2D.OverlapArea(groundCheckTopLeft.position, groundCheckBottomRight.position, layerMask);
 
-        horizontalMovement = Input.GetAxis("Horizontal");
-
-        jumpManagement();
+        if (movable)
+        {
+            horizontalMovement = Input.GetAxis("Horizontal");
+            jumpManagement();
+            changeCharacterManagement();
+        } else
+        {
+            horizontalMovement = 0;
+        }
 
         if (isGrounded)
         {
             playLanding();
             extraJumpsAvailable = extraJumps;
-        }
-        else
+        } else
         {
             landingPlayed = false;
         }
@@ -65,55 +78,64 @@ public abstract class PlayerController : MonoBehaviour {
         if (!isOnFocus)
         {
             focusIndicator.SetActive(false);
+            movable = false;
+        } else
+        {
+            focusIndicator.SetActive(true);
+            movable = true;
         }
     }
 
     protected void FixedUpdate()
     {
-        if (movable)
+       rb.velocity = new Vector2(horizontalMovement * speed, rb.velocity.y);
+    }
+
+    protected void changeCharacterManagement()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            rb.velocity = new Vector2(horizontalMovement * speed, rb.velocity.y);
+            isOnFocus = false;
+            int actualFocus = transform.GetSiblingIndex();
+            transform.parent.GetChild((actualFocus + 1) % transform.parent.childCount).GetComponent<PlayerController>().setOnFocus();
         }
     }
 
     protected void jumpManagement()
     {
-        if (movable)
+        if (Input.GetButton("Jump"))
         {
-            if (Input.GetButton("Jump"))
+            if (isGrounded)
             {
-                if (isGrounded)
+                jumping = true;
+                if (Input.GetButtonDown("Jump"))
+                    playerAnimation.Play(characterName + "Jump");
+            }
+            else if (extraJumpsAvailable > 0)
+            {
+                if (Input.GetButtonDown("Jump"))
                 {
                     jumping = true;
-                    if (Input.GetButtonDown("Jump"))
-                        playerAnimation.Play(characterName + "Jump");
+                    playerAnimation.Stop(characterName + "Jump"); // si double saut rapide
+                    playerAnimation.Play(characterName + "Jump");
+                    extraJumpsAvailable--;
                 }
-                else if (extraJumpsAvailable > 0)
-                {
-                    if (Input.GetButtonDown("Jump"))
-                    {
-                        jumping = true;
-                        playerAnimation.Stop(characterName + "Jump"); // si double saut rapide
-                        playerAnimation.Play(characterName + "Jump");
-                        extraJumpsAvailable--;
-                    }
-                }
+            }
 
-                if(jumping && jumpTimeCounter > 0)
-                {
-                    rb.velocity = new Vector2(rb.velocity.x, 1 * jumpForce);
-                    jumpTimeCounter -= Time.deltaTime;
-                }
-            }
-            else if (Input.GetButtonUp("Jump"))
+            if(jumping && jumpTimeCounter > 0)
             {
-                jumpTimeCounter = jumpTime;
-                jumping = false;
+                rb.velocity = new Vector2(rb.velocity.x, 1 * jumpForce);
+                jumpTimeCounter -= Time.deltaTime;
             }
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            jumpTimeCounter = jumpTime;
+            jumping = false;
         }
     }
 
-    protected void playLanding()
+    protected void playLanding() // voir les onLandEvent...
     {
         if (!landingPlayed && !onGoal)
         {
@@ -122,26 +144,55 @@ public abstract class PlayerController : MonoBehaviour {
         }
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    protected void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject == goal)
         {
+            spriteRenderer.color = new Color(1, 1, 1, 1);
             onGoal = true;
+            if (checkIfAllOnGoal())
+            {
+                movable = false;
+                rb.velocity = Vector2.zero;
+                rb.gravityScale = 0;
+                focusIndicator.SetActive(false);
+                StartCoroutine(moveToGoal(transform, goal.transform.position, moveToGoalSpeed));
+            }
         }
     }
 
-    protected void OnTriggerEnter2D(Collider2D collision)
+    protected void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject == goal)
+        if (collision.gameObject == goal && !checkIfAllOnGoal())
         {
-            movable = false;
-            rb.velocity = Vector2.zero;
-            rb.gravityScale = 0;
-            StartCoroutine(MoveTo(transform, goal.transform.position, 0.7f));
+            spriteRenderer.color = initialColor;
         }
     }
 
-    protected IEnumerator MoveTo(Transform toMove, Vector2 destination, float speed)
+    protected bool checkIfAllOnGoal()
+    {
+        foreach (Transform character in transform.parent)
+        {
+            PlayerController characterScript = character.GetComponent<PlayerController>();
+            if (!characterScript.isOnGoal())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected bool isOnGoal()
+    {
+        return onGoal;
+    }
+
+    protected void setOnFocus()
+    {
+        isOnFocus = true;
+    }
+
+    protected IEnumerator moveToGoal(Transform toMove, Vector2 destination, float speed)
     {
         while((Vector2) toMove.position != destination)
         {
