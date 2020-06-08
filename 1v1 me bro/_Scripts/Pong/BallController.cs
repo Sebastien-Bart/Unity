47ebd05d941e65aeae69c7d72f6d3518e16b4579
Sebastien-Bart <45076193+Sebastien-Bart@users.noreply.github.com>
2020-06-08 21:56:46 +0200

    Update BallController.cs
    Segmentation des raquettes + particules contact balle

--- 1v1 me bro/_Scripts/Pong/BallController.cs
+++ 1v1 me bro/_Scripts/Pong/BallController.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
@@ -11,10 +11,13 @@ public class BallController : MonoBehaviour
     public PlayerController rightPlayer;
 
     [Header("Ball Settings")]
-    public float speed;
+    public ParticleSystem hitParticle;
+    public float startSpeed;
+    [Range(1f, 1.2f)] public float acceleration;
 
     private Rigidbody2D rb;
     private SpriteRenderer sr;
+    private Vector2 prevVel;
 
     void Start()
     {
@@ -25,31 +28,85 @@ public class BallController : MonoBehaviour
 
     public void KickOff()
     {
-        float x = Random.Range(3f, speed - 0.5f);
-        float y = Mathf.Sqrt(speed * speed - x * x);
-        if ((int)Random.Range(0f, 2f) == 1)
-            x *= -1;
-        if ((int)Random.Range(0f, 2f) == 1)
-            y *= -1;
-        rb.velocity = new Vector2(x, y);
+        prevVel = Vector2.zero;
+        int x = 1;
+        if ((int)UnityEngine.Random.Range(0,2) == 0)
+            x = -1;
+        rb.velocity = new Vector2(x*startSpeed, 0);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        PlayHitParticleAccordingToCollission(collision);
         if (collision.collider.tag == "raquette")
         {
-            // float tranche = collision.collider.transform.localScale.y / 8;
-            // float segment = Mathf.RoundToInt((transform.position.y - collision.collider.transform.position.y) / tranche);
-            // Debug.Log(segment);
-            rb.velocity *= 1.20f;
+            CalculateNewTrajectory(collision);
         }
-
         else if (collision.collider.tag == "but")
         {
             rb.velocity = Vector2.zero;
             StartCoroutine("Goal");
         }
-    }   
+    }
+
+    private void PlayHitParticleAccordingToCollission(Collision2D collision)
+    {
+        ParticleSystem particle = Instantiate(hitParticle);
+        Vector2 contactPos = collision.GetContact(0).point;
+        particle.transform.position = contactPos;
+
+        float xMin = contactPos.x - 0.2f;
+        float xMax = contactPos.x + 0.2f;
+
+        if (transform.position.x < xMax && transform.position.x > xMin) // collision sur mur haut ou bas
+        {
+            if (transform.position.y < contactPos.y)
+                particle.transform.localRotation = Quaternion.Euler(-270, 0, 0);
+            else if (transform.position.y > contactPos.y)
+                particle.transform.localRotation = Quaternion.Euler(-90, 0, 0);
+        }
+        else
+        {
+            if (transform.position.x > contactPos.x)
+                particle.transform.localRotation = Quaternion.Euler(0, 90, 0);
+            if (transform.position.x < contactPos.x)
+                particle.transform.localRotation = Quaternion.Euler(0, 270, 0);
+        }
+        particle.Play();
+    }
+
+    private void CalculateNewTrajectory(Collision2D collision)
+    {
+        float nextMagnitudeSqr = 0;
+        if (prevVel.sqrMagnitude > rb.velocity.sqrMagnitude) // si mauvaise collision (coin de raquette)
+            nextMagnitudeSqr = (prevVel * acceleration * acceleration).sqrMagnitude;
+        else
+            nextMagnitudeSqr = (rb.velocity * acceleration).sqrMagnitude;
+
+        float tranche = collision.collider.transform.localScale.y / 8;
+        float segment = Mathf.RoundToInt((transform.position.y - collision.collider.transform.position.y) / tranche);
+
+        int xDirection = 1;
+        int yDirection = 1;
+        if (segment < 0)
+            yDirection = -1;
+        if (transform.position.x > 0)
+            xDirection = -1;
+
+        float angle = 0;
+        if (Mathf.Abs(segment) == 1)
+            angle = 15;
+        else if (Mathf.Abs(segment) == 2)
+            angle = 30;
+        else if (Mathf.Abs(segment) >= 3)
+            angle = 45;
+
+        float xVel = xDirection * Mathf.Cos(angle * Mathf.Deg2Rad) * Mathf.Sqrt(nextMagnitudeSqr);
+        float yVel = yDirection * Mathf.Sin(angle * Mathf.Deg2Rad) * Mathf.Sqrt(nextMagnitudeSqr);
+
+        rb.velocity = new Vector2(xVel, yVel);
+        prevVel = new Vector2(xVel, yVel);
+    }
 
     private IEnumerator Goal()
     {
